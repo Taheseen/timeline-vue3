4143fb8eeaa0fb2f8ae9b685478f07b221fca1a7
Tahesin <tahesinara.t@gmail.com>
2024-02-14 21:37:31 +0100

    feat: Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    This is a simple app built with Vue3. It allows you to new events and see them on your timeline.
+  </main>
+</template>
